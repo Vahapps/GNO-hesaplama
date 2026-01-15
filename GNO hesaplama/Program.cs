b0127e923e240994d62a6d0aa7d2597177f57a97
Abdulvahap Gültekin <gultekin11010110@gmail.com>
2026-01-15 15:42:12 +0300

    Update Program.cs

--- GNO hesaplama/Program.cs
+++ GNO hesaplama/Program.cs
@@ -13,7 +13,7 @@ for (int i = 0; i < ders_sayısı; i++)
     int vize_notu = int.Parse(Console.ReadLine());
     Console.Write(ders_adi+" final notunuz = ");
     int final_notu = int.Parse(Console.ReadLine());
-    double ders_notu = (vize_notu * 40 / 100) + (final_notu * 60 / 100);
+    double ders_notu = (vize_notu * 0.4) + (final_notu * 0.6);
     Console.Clear();
     Console.WriteLine(ders_adi+" ders notunuz = " + ders_notu);
     Console.WriteLine("********************************* \n ");
@@ -25,6 +25,7 @@ double ortalama = (double)kredisi_hesaplanmış_puan.Sum() / toplam_kredi;
 double GNO_puanı = ortalama / 25;
 Console.WriteLine("GNO'nuz = " + GNO_puanı);
 Console.ReadKey();
+
 
 
 
